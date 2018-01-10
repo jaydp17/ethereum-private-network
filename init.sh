ce066a799271286f26254978f8cff5c70b4b1e88
Jaydeep Solanki <jaydp17@gmail.com>
2018-01-10 23:02:25 +0530

    make node 2 the miner

--- init.sh
+++ init.sh
@@ -3,14 +3,16 @@
 # creates a new account and saves in the keystore
 get_new_account() {
   # new_account_output="Address: {af5e8bb52b243d5ce616e5eb11acb8b10fae9fc3}"
-  new_account_output=$(docker run --rm --name account-generator -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node1:/root/.ethereum gethd:latest geth account new --password /base-config/init-password.txt)
+  new_account_output=$(docker run --rm --name account-generator -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/$1:/root/.ethereum gethd:latest geth account new --password /base-config/init-password.txt)
   echo $new_account_output | cut -b11-50
 }
 
-addr1=$(get_new_account)
-addr2=$(get_new_account)
-addr3=$(get_new_account)
-addr4=$(get_new_account)
+addr1=$(get_new_account node1)
+addr2=$(get_new_account node1)
+addr3=$(get_new_account node1)
+addr4=$(get_new_account node1)
+addr5=$(get_new_account node2)
+addr6=$(get_new_account node2)
 
 # put all the hashes in the genesis.json file
 template_path="./base-config/genesis-template.json"
@@ -20,6 +22,8 @@ sed -i s/__ADDR1__/$addr1/g $genesis_path
 sed -i s/__ADDR2__/$addr2/g $genesis_path
 sed -i s/__ADDR3__/$addr3/g $genesis_path
 sed -i s/__ADDR4__/$addr4/g $genesis_path
+sed -i s/__ADDR5__/$addr5/g $genesis_path
+sed -i s/__ADDR6__/$addr6/g $genesis_path
 
 # initialize the chain
 docker-compose -f docker-compose.init.yml up
