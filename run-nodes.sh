70a4afb8a1b8bf3822b460fc5566f7cfd2c7cbd5
Jaydeep Solanki <jaydp17@gmail.com>
2018-01-10 15:48:40 +0530

    add a script to run nodes

---
+++ run-nodes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# # create a network
+# docker network create ETH
+
+# # create the boot key
+# docker run --rm --name boot-key -v ${PWD}/boot:/boot gethd:latest bootnode --genkey=/boot/boot.key
+
+# # initialize the chain
+# docker run --rm --name first-node-init -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node1:/root/.ethereum gethd:latest geth init /base-config/genesis.json
+# docker run --rm --name second-node-init -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node2:/root/.ethereum gethd:latest geth init /base-config/genesis.json
+
+# # start geth
+# docker run --name first-node -d --network ETH -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node1:/root/.ethereum gethd:latest geth --identity="node-one" --networkid="7" --rpcaddr 0.0.0.0
+# docker run --name second-node -d --network ETH -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node2:/root/.ethereum gethd:latest geth --identity="node-two" --networkid="7" --rpcaddr 0.0.0.0
+
+docker-compose up -d
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker run --name first-node -t -p 30303:30303 -v ${PWD}/base-config:/base-config -v ${PWD}/node1:/root/.ethereum gethd:latest geth init /base-config/genesis.json
-
