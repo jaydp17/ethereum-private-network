8a7dbb5c28add57b3ec382771e1f8b5a2b2e39bf
Jaydeep Solanki <jaydp17@gmail.com>
2018-01-10 16:26:16 +0530

    improve run-nodes script

--- run-nodes.sh
+++ run-nodes.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+
+#################################################
+########   Below is the canonical way to do it
+#################################################
+
 # # create a network
 # docker network create ETH
 
 # # create the boot key
 # docker run --rm --name boot-key -v ${PWD}/boot:/boot gethd:latest bootnode --genkey=/boot/boot.key
+# docker run --rm --name boot-key -v ${PWD}/boot:/boot gethd:latest bootnode --nodekey=/boot/boot.key
 
 # # initialize the chain
 # docker run --rm --name first-node-init -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node1:/root/.ethereum gethd:latest geth init /base-config/genesis.json
@@ -14,4 +20,17 @@
 # docker run --name first-node -d --network ETH -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node1:/root/.ethereum gethd:latest geth --identity="node-one" --networkid="7" --rpcaddr 0.0.0.0
 # docker run --name second-node -d --network ETH -v ${PWD}/base-config:/base-config -v ${PWD}/nodes-data/node2:/root/.ethereum gethd:latest geth --identity="node-two" --networkid="7" --rpcaddr 0.0.0.0
 
+
+
+######################################################
+########   Below is the docker-compose way to do it
+######################################################
+
+# start the containers
 docker-compose up -d
+
+# wait for them to boot up
+sleep 2
+
+# attach
+./attach-first-node.sh
